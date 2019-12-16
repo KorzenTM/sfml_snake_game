9c8738d6dca64e1fa967f05a4bdbf52f127c2ea1
Mateusz Korzeniowski <56135913+KorzenTM@users.noreply.github.com>
2019-12-16 13:36:17 +0100

    Add files via upload
    Create window

---
+++ src/game.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+#define WIDTH 800
+#define HEIGHT 600
+
+class Game
+{
+    public:
+    Game();
+    void run();
+
+    private:
+    void processEvents();
+    void update();
+    void render();
+
+    sf::RenderWindow window;
+};
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "game.h"
+#include "game.cpp"
+
+int main()
+{
+    Game game;
+    game.run();
+}
